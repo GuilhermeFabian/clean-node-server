eac7fa277a874c89c2a3f7ea1cc97c1ea093586b
Fabian <guilhermeferreirafabian@gmail.com>
2022-04-09 17:05:56 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -24,4 +24,13 @@ describe("EmailValidator Adapter", () => {
 
     expect(isValid).toEqual(true);
   });
+
+  test("Should call validator with correct email", () => {
+    const sut = new EmailValidatorAdapter();
+    const isEmailSpy = jest.spyOn(validator, "isEmail");
+
+    sut.isValid("any_email@mail.com");
+
+    expect(isEmailSpy).toBeCalledWith("any_email@mail.com");
+  });
 });
