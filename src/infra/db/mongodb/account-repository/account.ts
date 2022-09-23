c169c67eeaf1c03fbf22c2af34aad6391913a85b
Fabian <guilhermeferreirafabian@gmail.com>
2022-09-23 04:23:50 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,29 +1,15 @@
 import { AddAccountRepository } from "@/data/protocols/add-account-repository";
 import { AccountModel } from "@/domain/models/account";
 import { AddAccountModel } from "@/domain/usecases/add-account";
-import { MongoHelper } from "../helpers/mongo-helper";
-
-interface MongoAccountModel extends Omit<AccountModel, "id"> {
-  _id: string;
-}
+import { MongoHelper, MongoCollection } from "../helpers/mongo-helper";
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection("account");
     const { insertedId } = await accountCollection.insertOne(accountData);
+    const response = await accountCollection.findOne<MongoCollection>(insertedId);
 
-    const response = await accountCollection.findOne<MongoAccountModel>(insertedId);
-
-    if (response) {
-      const { _id, ...accountWithoutId } = response;
-      const account = {
-        id: _id,
-        ...accountWithoutId,
-      } as AccountModel;
-
-      return account;
-    }
-
+    if (response) return MongoHelper.map<AccountModel>(response);
     return Promise.reject();
   }
 }
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,5 +1,10 @@
 import { Collection, MongoClient } from "mongodb";
 
+export interface MongoCollection {
+  _id: string;
+  [key: string]: unknown;
+}
+
 export const MongoHelper = {
   client: null as unknown as MongoClient,
 
@@ -13,5 +18,15 @@ export const MongoHelper = {
 
   getCollection(name: string): Collection {
     return this.client.db().collection(name);
+  },
+
+  map<Model extends { id: string }>(collection: MongoCollection): Model {
+    const { _id, ...collectionWithoutId } = collection;
+    const model = {
+      id: _id,
+      ...collectionWithoutId,
+    } as Model;
+
+    return model;
   },
 };
